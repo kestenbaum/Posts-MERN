409003788927a0c79ac756adb453537b2bf363c2
Oleksii Vovnenko <kespa71for@gmail.com>
2025-02-09 20:15:40 +0100

    create: model Post

---
+++ backend/src/models/Post.js
@@ -0,0 +1,9 @@
+import { model, Schema } from "mongoose";
+
+const postSchema = new Schema({
+		title: {type: String, required: true},
+		description: {type: String, required: true},
+})
+
+const Post = model('Post', postSchema);
+export default Post;
