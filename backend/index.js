80b3410dfa42dec397d1b35ab45040742b73a907
Oleksii Vovnenko <kespa71for@gmail.com>
2025-02-10 07:48:29 +0100

    update: index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import CONNECT_DB from "./src/config/index.js";
 import dotenv from "dotenv";
+import router  from "./src/routes/postRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use("/api/posts", router);
 
 async function startApp () {
 		try {
